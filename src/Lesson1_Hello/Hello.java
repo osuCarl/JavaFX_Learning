c7d5eddd64f503a54806f0087d82f655928abc16
osuCARL <j605699643@outlook.com>
2023-09-09 15:14:05 +1000

    Refactor Hello

--- src/Lesson1_Hello/Hello.java
+++ src/Lesson1_Hello/Hello.java
@@ -1,9 +1,11 @@
+package Lesson1_Hello;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Lesson1_Hello.Hello world!");
         launch(args);
     }
 
